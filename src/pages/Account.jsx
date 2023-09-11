3537c06d6374adb92bab9e56ab87a9b384ea0fe0
Bryan Pike <107475188+thebobfather2@users.noreply.github.com>
2023-09-11 09:06:14 -0400

    Update Account.jsx

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -14,6 +14,48 @@ const Account = () => {
     }
   };
 
+  import React, { useState, useEffect } from 'react';
+import { UserAuth } from '../context/AuthContext';
+import firebase from '../firebase'; // Import your Firebase configuration
+
+const Account = () => {
+  const { user } = UserAuth();
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  // Function to send a message to Firebase
+  const sendMessage = async () => {
+    if (message) {
+      const messageData = {
+        text: message,
+        timestamp: firebase.database.ServerValue.TIMESTAMP,
+        sender: user.displayName,
+      };
+
+      await firebase.database().ref('messages').push(messageData);
+      setMessage('');
+    }
+  };
+
+  // Function to fetch messages from Firebase
+  useEffect(() => {
+    const messagesRef = firebase.database().ref('messages');
+
+    messagesRef.on('value', (snapshot) => {
+      const messageList = [];
+      snapshot.forEach((childSnapshot) => {
+        const messageData = childSnapshot.val();
+        messageList.push(messageData);
+      });
+
+      // Update the state with the messages
+      setMessages(messageList);
+    });
+
+    // Clean up the listener when the component unmounts
+    return () => messagesRef.off('value');
+  }, []);
+
   return (
     <div className='accountBox'>
       <h1 className='hello'>Deep Notes</h1>
@@ -21,12 +63,28 @@ const Account = () => {
         <p className='personalGreeting'>Welcome, {user?.displayName}</p>
         <br></br>
         <Forum user={user} />
-        <div className='updatesBox'>
-          <br></br>
-          <h2>PROTECTED CONTENT</h2>
-          <br></br>
-          <p>Sensitive information will be posted here about members only content.</p>
-        </div>
+         <div className='updatesBox'>
+        {/* Display messages */}
+        {messages.map((messageData, index) => (
+          <div key={index}>
+            <p>{messageData.sender}: {messageData.text}</p>
+          </div>
+        ))}
+
+        <br />
+        <h2>PROTECTED CONTENT</h2>
+        <br />
+        <p>Sensitive information will be posted here about members-only content.</p>
+      </div>
+        <div>
+        <input
+          type='text'
+          placeholder='Type your message'
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
       </div>
       <button onClick={handleSignOut} className='logoutBtn'>
         Logout
